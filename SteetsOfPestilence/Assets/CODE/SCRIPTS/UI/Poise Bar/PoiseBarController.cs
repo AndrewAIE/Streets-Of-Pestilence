20f25ce4c49f948e839eda9f3a7214f1318ad02d
Ru McPharlin <rupertmcpharlin@gmail.com>
2024-09-11 16:01:44 +0930

    Slider v2
    Added[UI art folerder]
    Added[Background, fill, outline plunger sprites]
    Added[Poise bar controller script]
    Added[Poise bar prefab]

---
+++ SteetsOfPestilence/Assets/CODE/SCRIPTS/UI/Poise Bar/PoiseBarController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoiseBarController : MonoBehaviour
+{
+    [SerializeField, Range(0,1)] float _fillAmount;
+
+    [Header("Blood Fill")]
+    [SerializeField] RectMask2D _fillMask;
+    [SerializeField] float _maskPlayerWin;
+    [SerializeField] float _maskEnemyWin;
+
+    [Header("Plunger")]
+    [SerializeField] RectTransform _plungerTransform;
+    [SerializeField] float _plungerX_PlayerWin;
+    [SerializeField] float _plungerX_EnemyWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _fillMask.padding = new Vector4((_maskEnemyWin - _maskPlayerWin) * _fillAmount, 0, 0, 0);
+
+        _plungerTransform.anchoredPosition = new Vector3(Mathf.Lerp(_plungerX_PlayerWin, _plungerX_EnemyWin, _fillAmount), 0, 0);
+
+    }
+
+
+}
