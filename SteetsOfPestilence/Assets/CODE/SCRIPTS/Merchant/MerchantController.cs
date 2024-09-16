7d31d074584943c9b463367108b8a40fe627e039
Ru McPharlin <rupertmcpharlin@gmail.com>
2024-09-16 11:20:14 +0930

    Push For Merge

--- SteetsOfPestilence/Assets/CODE/SCRIPTS/Merchant/MerchantController.cs
+++ SteetsOfPestilence/Assets/CODE/SCRIPTS/Merchant/MerchantController.cs
@@ -4,96 +4,99 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class MerchantController : MonoBehaviour
+namespace Merchant
 {
-    /***************************** VARIABLES ****************************/
-    #region Variables
-
-    #region Merchant State
-    public enum MerchantState
+    public class MerchantController : MonoBehaviour
     {
-        Idle,
-        InRange,
-        InShop
-    }
+        /***************************** VARIABLES ****************************/
+        #region Variables
 
-    [Header("Merchant State")]
-    public MerchantState _merchantState;
-
-    #endregion
-
-
-
-
-    /*** UI ***/
-    #region UI
-    [Header("UI")]
-    [SerializeField] TMP_Text _interactText;
-
-    #endregion
-
-    /*** COMPONENTS ***/
-    #region Components
-    [HideInInspector] PlayerManager _player;
-    [HideInInspector] CameraController _camera;
-
-    #endregion
-
-
-
-    #endregion
-
-    private void Awake()
-    {
-        _player = FindObjectOfType<PlayerManager>();
-        _camera = FindObjectOfType<CameraController>();
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
+        #region Merchant State
+        public enum MerchantState
         {
-            //turn on text
-            _interactText.gameObject.SetActive(true);
-
-            //set state
-            SetMerchantState(MerchantState.InRange);
+            Idle,
+            InRange,
+            InShop
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Player"))
+        [Header("Merchant State")]
+        public MerchantState _merchantState;
+
+        #endregion
+
+
+
+
+        /*** UI ***/
+        #region UI
+        [Header("UI")]
+        [SerializeField] TMP_Text _interactText;
+
+        #endregion
+
+        /*** COMPONENTS ***/
+        #region Components
+        [HideInInspector] PlayerManager _player;
+        [HideInInspector] CameraController _camera;
+
+        #endregion
+
+
+
+        #endregion
+
+        private void Awake()
         {
-            //turn off text
-            _interactText.gameObject.SetActive(false);
-
-            //Set Merchant State
-            SetMerchantState(MerchantState.Idle);
+            _player = FindObjectOfType<PlayerManager>();
+            _camera = FindObjectOfType<CameraController>();
         }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                //turn on text
+                _interactText.gameObject.SetActive(true);
+
+                //set state
+                SetMerchantState(MerchantState.InRange);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                //turn off text
+                _interactText.gameObject.SetActive(false);
+
+                //Set Merchant State
+                SetMerchantState(MerchantState.Idle);
+            }
+        }
+
+        /*** MERCHANT STATE ***/
+        #region Merchant State
+
+        public void SetMerchantState(MerchantState inputState)
+        {
+            _merchantState = inputState;
+        }
+
+
+        #endregion
     }
-
-    /*** MERCHANT STATE ***/
-    #region Merchant State
-
-    public void SetMerchantState(MerchantState inputState)
-    {
-        _merchantState = inputState;
-    }
-
-
-    #endregion
 }
