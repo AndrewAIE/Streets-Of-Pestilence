c93471d414f459065d20115632b4023f05b15ed1
AndrewAIE <126208967+AndrewAIE@users.noreply.github.com>
2024-10-15 16:40:23 +1030

    Add[NewActionPopup]
    Hvae created editor scripts for enemy streams as well as pop up windows to create different types of actions

--- SteetsOfPestilence/Assets/PLAYER/QTESYSTEM/Editor/NewActionPopUp.cs
+++ SteetsOfPestilence/Assets/PLAYER/QTESYSTEM/Editor/NewActionPopUp.cs
@@ -1,25 +1,52 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
-using PopupWindow = UnityEditor.PopupWindow;
+using QTESystem;
+using Unity.VisualScripting;
 
 
 public class NewActionPopUp : EditorWindow
 {
+    static QTEStreamEditor m_editor;
     [MenuItem("Example/Popup Example")]
-    static public void Init()
+    static public void Init(QTEStreamEditor _editor)
     {
         EditorWindow window = CreateInstance<NewActionPopUp>();
-        window.Show();
+        m_editor = _editor;
+        window.Show();        
+    } 
+
+    public void SetEditor(QTEStreamEditor _editor)
+    {
+        m_editor = _editor;
     }
 
     public void CreateGUI()
     {
         var visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/PLAYER/QTESYSTEM/Editor/NewActionPopUp.uxml");
         visualTreeAsset.CloneTree(rootVisualElement);
+        
+        var buttonPress = rootVisualElement.Q<Button>("Press");
+        buttonPress.clicked += () => CreateAction(0);
 
-        var button = rootVisualElement.Q<Button>();
-        button.clicked += () => PopupWindow.Show(button.worldBound, new NewActionPopupContent());
+        var buttonHold = rootVisualElement.Q<Button>("Hold");
+        buttonHold.clicked += () => CreateAction(1);
+
+        var buttonMash = rootVisualElement.Q<Button>("Mash");
+        buttonMash.clicked += () => CreateAction(2);
+
+        var buttonSequence = rootVisualElement.Q<Button>("Sequence");
+        buttonSequence.clicked += () => CreateAction(3);
+    }
+
+    public void CreateAction(int _indicator)
+    {
+        Debug.Log("Create Action type " + _indicator);        
+        m_editor.AddAction(_indicator);
+    }
+
+    private void OnLostFocus()
+    {
+        this.Close();   
     }
 }
--- SteetsOfPestilence/Assets/PLAYER/QTESYSTEM/Editor/QTEStreamEditor.cs
+++ SteetsOfPestilence/Assets/PLAYER/QTESYSTEM/Editor/QTEStreamEditor.cs
@@ -5,15 +5,14 @@ using UnityEditor;
 using UnityEditor.UIElements;
 using QTESystem;
 using UnityEngine.UIElements;
+using System.IO;
 using Unity.VisualScripting;
-using System;
 
 [CustomEditor(typeof(QTEStreamData))]
 public class QTEStreamEditor : Editor
 {
     //Stream Data to be edited
     QTEStreamData m_streamData;
-    
     QTEAction m_action;
     
     //Visual Elements
@@ -22,13 +21,27 @@ public class QTEStreamEditor : Editor
     FloatField m_pauseAtStart;
     FloatField m_pauseAtEnd;
     FloatField m_successBuffer;
-    Button m_addNewAction;    
-    
+    Button m_addNewAction;
+    List<QTEAction> m_actionList;
+    List<Button> m_removeAction;
+    List<Button> m_moveActionUp;
+    List<Button> m_moveActionDown;
+    List<List<EnumField>> m_inputEnums;
+
+    string m_filePath = "PLAYER/QTESYSTEM/ScriptableObjects";
+    bool m_gotData = false;
+    int InputCount;
 
     private void OnEnable()
     {
         m_streamData = (QTEStreamData)target;
-        m_action = new QTEHoldAction();
+        m_gotData = false;
+        m_removeAction = new List<Button>();        
+        m_moveActionUp = new List<Button>();
+        m_moveActionDown = new List<Button>();
+        m_inputEnums = new List<List<EnumField>>();
+        m_actionList = new List<QTEAction>();
+        InputCount = 0;
     }
 
 
@@ -41,10 +54,11 @@ public class QTEStreamEditor : Editor
         m_successBuffer = new FloatField();
         m_addNewAction = new Button();
         m_addNewAction.text = "Add New Action";
-        m_addNewAction.clicked += CreateAction;
-        //GetActionList();
+        m_addNewAction.clicked += CreateActionWindow;
+        GetData();
+        //Get Time Data from actions and apply them to 
         customInspector.Add(new Label("QTE Stream Creator"));
-        customInspector.Add(new Label("Time per Action (in seconds)"));
+        customInspector.Add(new Label("QTE Metronome (in seconds)"));
         customInspector.Add(m_timePerAction);
         customInspector.Add(new Label("Pause At Start (in seconds)"));
         customInspector.Add(m_pauseAtStart);
@@ -53,16 +67,39 @@ public class QTEStreamEditor : Editor
         customInspector.Add(new Label("Success Window (in seconds)"));
         customInspector.Add(m_successBuffer);
         customInspector.Add(new Label("Action List"));
-        customInspector.Add(m_action);
-        customInspector.Add(m_addNewAction);
-        GetData();
+        for(int i = 0; i < m_actionList.Count; i++)
+        {
+            customInspector.Add(new Label("Action " + i + " data"));
+            customInspector.Add(new Label("Input List"));
+            //Get Data and create input lists from actions
+            m_inputEnums.Add(new List<EnumField>());
+            for (int j = 0; j < m_actionList[i].InputList.Count; j++)
+            {
+                m_inputEnums[i].Add(new EnumField(m_actionList[i].InputList[j]));
+                customInspector.Add(m_inputEnums[i][j]);
+                InputCount++;
+            }
+            //Create Buttons for each Action
+            m_moveActionUp.Add(new Button());            
+            m_moveActionUp[i].text = "Move Action Up";
+            m_moveActionUp[i].clicked += () => MoveActionUp(i - 1);
+            customInspector.Add(m_moveActionUp[i]);
+            m_moveActionDown.Add(new Button());
+            m_moveActionDown[i].text = "Move Action Down";
+            m_moveActionDown[i].clicked += () => MoveActionDown(i - 1);
+            customInspector.Add(m_moveActionDown[i]);
+            m_removeAction.Add(new Button());
+            m_removeAction[i].text = "Remove Action";
+            m_removeAction[i].clicked += () => RemoveAction(i - 1);
+            customInspector.Add(m_removeAction[i]);            
+        }
+        customInspector.Add(m_addNewAction);        
         return customInspector;
     }
 
     public void OnValidate()
     {
-        
-            
+        //SetData();            
     }
 
     public void GetData()
@@ -71,48 +108,86 @@ public class QTEStreamEditor : Editor
         m_pauseAtStart.value = m_streamData.BeginningOfStreamPause;
         m_pauseAtEnd.value = m_streamData.EndOfStreamPause;
         m_successBuffer.value = m_streamData.SuccessBuffer;
+        for(int i = 0; i < m_streamData.Actions.Count; i++)
+        {
+            m_actionList.Add(m_streamData.Actions[i]);
+        }        
+        m_gotData = true;
     }
 
 
     public void SetData()
     {
-        m_streamData.ActionTimer = m_timePerAction.value;
-        m_streamData.BeginningOfStreamPause = m_pauseAtStart.value;
-        m_streamData.EndOfStreamPause = m_pauseAtEnd.value;
-        m_streamData.SuccessBuffer = m_successBuffer.value;
+        if(m_gotData)
+        {
+            m_streamData.ActionTimer = m_timePerAction.value;
+            m_streamData.BeginningOfStreamPause = m_pauseAtStart.value;
+            m_streamData.EndOfStreamPause = m_pauseAtEnd.value;
+            m_streamData.SuccessBuffer = m_successBuffer.value;
+            m_streamData.Actions.Clear();
+            for(int i = 0; i < m_actionList.Count; i++)
+            {
+                m_streamData.Actions.Add(m_actionList[i]);
+            }
+        }        
+    }
+   
+
+    public void CreateActionWindow()
+    {
+        CreateInstance<NewActionPopUp>();        
+        NewActionPopUp.Init(this);        
     }
 
-    void GetActionList()
+    public void AddAction(int _indicator)
     {
-        //m_actionList.Clear();
-        //for(int i = 0; i < streamData.Actions.Count; i++)
-        //{
-        //    m_actionList.Add(streamData.Actions[i]);
-        //}
+        string filePath = Application.persistentDataPath + "/" + m_filePath + "/" + m_streamData.name + "/";
+        Debug.Log(filePath);
+        Directory.CreateDirectory(filePath);
+        filePath = "Assets/" + m_filePath + "/" + m_actionList.Count + ".asset";
+        switch(_indicator)
+        {
+            case 0:
+                AssetDatabase.CreateAsset(CreateInstance<QTEPressAction>(), filePath);
+                QTEPressAction pressAction = AssetDatabase.LoadAssetAtPath<QTEPressAction>(filePath);
+                m_actionList.Add(pressAction);
+                break;
+            case 1:
+                AssetDatabase.CreateAsset(CreateInstance<QTEHoldAction>(), filePath);
+                QTEHoldAction holdAction = AssetDatabase.LoadAssetAtPath<QTEHoldAction>(filePath);
+                m_actionList.Add(holdAction);
+                break;
+            case 2:
+                AssetDatabase.CreateAsset(CreateInstance<QTEMashAction>(), filePath);
+                QTEMashAction mashAction = AssetDatabase.LoadAssetAtPath<QTEMashAction>(filePath);
+                m_actionList.Add(mashAction);
+                break;
+            case 3:
+                AssetDatabase.CreateAsset(CreateInstance<QTESequenceAction>(), filePath);
+                QTESequenceAction sequenceAction = AssetDatabase.LoadAssetAtPath<QTESequenceAction>(filePath);
+                m_actionList.Add(sequenceAction);
+                break;                
+        }        
+    }  
+
+    public void RemoveAction(int _index)
+    {
+        Debug.Log("Size Of List = " + m_removeAction.Count);
+        Debug.Log("Trying To Remove At " + _index);
+        m_removeAction.RemoveAt(_index);
+        m_moveActionUp.RemoveAt(_index);
+        m_moveActionDown.RemoveAt(_index);
+        m_inputEnums.RemoveAt(_index);
+        m_actionList.RemoveAt(_index);
     }
 
-    void SetActionList()
-    {
-        //streamData.Actions.Clear();
-        //for(int i = 0; i < m_actionList.Count; i++)
-        //{
-        //    streamData.Actions.Add(m_actionList[i]);
-        //}
-    }
-
-    public void CreateAction()
-    {
-        CreateInstance<NewActionPopUp>();
-        NewActionPopUp.Init();
-    }
-
-    public void AddAction(QTEAction _action)
+    public void MoveActionUp(int _index)
     {
 
     }
 
-    public void RemoveAction(QTEAction _action)
+    public void MoveActionDown(int _index)
     {
-
+        
     }
 }
