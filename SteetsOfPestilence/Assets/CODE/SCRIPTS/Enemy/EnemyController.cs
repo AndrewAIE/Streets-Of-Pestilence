888c8bcfca38cd4e350e0b51d2439bd3c08ab5a9
Oskar <s200080@students.aie.edu.au>
2024-10-28 16:51:17 +1030

    add [death particles]

--- SteetsOfPestilence/Assets/CODE/SCRIPTS/Enemy/EnemyController.cs
+++ SteetsOfPestilence/Assets/CODE/SCRIPTS/Enemy/EnemyController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayerController;
 using UnityEngine.AI;
+using Pixelplacement.TweenSystem;
 
 namespace EnemyAI
 {
@@ -22,7 +23,8 @@ namespace EnemyAI
         private PlayerManager m_player;
         public EnemyType m_EType;
 
-
+        [SerializeField]private bool isDead = false;
+        public bool recenter;
         #region Nav
         private NavMeshAgent m_agent;
         private EnemyDetector m_detector;
@@ -35,6 +37,11 @@ namespace EnemyAI
         [SerializeReference] private float m_waitTime = 10, m_timer;
         #endregion
 
+        #region Mesh & Particles
+        private ParticleSystem[] m_enemyPartycles;
+        private SkinnedMeshRenderer m_enemyMesh;
+        #endregion
+
 
 
         private void Awake()
@@ -43,29 +50,54 @@ namespace EnemyAI
             m_agent = GetComponentInParent<NavMeshAgent>();
             m_detector = GetComponent<EnemyDetector>();
 
+            m_enemyPartycles = transform.parent.GetComponentsInChildren<ParticleSystem>();
+            m_enemyMesh = transform.parent.GetComponentInChildren<SkinnedMeshRenderer>();
+
+
             m_homeDestination = transform.position;
         }
 
+        int died = 0;
         private void Update()
         {
-            if (!m_player.PlayerInCombat())
+            if (!isDead)
             {
-                if (m_detector.m_canSeePlayer)
-                    m_timer = m_waitTime;
-
-                if (m_timer > 0)
+                if (!m_player.PlayerInCombat())
                 {
-                    GoToPlayer();
-                    m_timer -= Time.deltaTime;
+                    if (m_detector.m_canSeePlayer)
+                        m_timer = m_waitTime;
+
+                    if (m_timer > 0)
+                    {
+                        GoToPlayer();
+                        m_timer -= Time.deltaTime;
+                    }
+                    else
+                        Standby();
                 }
-                else
-                    Standby();
             }
+            else if (died < 1) KillEnemy();
         }
-        private void OnDestroy()
+        public void KillEnemy()
         {
+            died++;
+            m_agent.enabled = false;
+            m_enemyMesh.enabled = false;
+            for (int i = 0; i <= m_enemyPartycles.Length; i++) m_enemyPartycles[i].Play();
+            Collider[] colliders = GetComponents<Collider>();
+            foreach(Collider col in colliders)
+            {
+                col.enabled = false;
+            }
+            StartCoroutine(WaitForDestroy());
+        }
+
+        private IEnumerator WaitForDestroy()
+        {
+            yield return new WaitForSeconds(10);
             Destroy(gameObject.transform.parent.gameObject);
         }
+
 
         #region Nav
         private void GoToPlayer()
@@ -74,6 +106,7 @@ namespace EnemyAI
             if (m_detector.EnemyIsClose() && !m_player.PlayerInCombat())
             {
                 FacePlayer();
+                RecenterEnemy();
                 m_player.EnterCombat(m_EncounterData, this);
                 return;
             }
@@ -104,10 +137,22 @@ namespace EnemyAI
 
         }
         #endregion
+        #region Encounter
 
         public void ForceEncounter()
         {
             m_player.EnterCombat(m_EncounterData, this);
         }
+        public void RecenterEnemy()
+        {
+            float range = 2f;
+            bool leftHit = Physics.SphereCast(transform.position, 1, Vector3.left, out RaycastHit leftInfo, range);
+            bool rightHit = Physics.SphereCast(transform.position, 1, Vector3.right, out RaycastHit rightInfo, range);
+
+
+        }
+
+
+        #endregion
     }
 }
