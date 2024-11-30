f50deedde5edbfce0e2be6fd7a3396bc43bc21c4
AndrewAIE <s214712@students.aie.edu.au>
2024-11-30 16:16:46 +1030

    ANIMATIONS

--- SteetsOfPestilence/Assets/PLAYER/QTESYSTEM/QTECombatAnimation.cs
+++ SteetsOfPestilence/Assets/PLAYER/QTESYSTEM/QTECombatAnimation.cs
@@ -42,43 +42,15 @@ public class QTECombatAnimation : MonoBehaviour
     public void SelectAnimation(int _poiseValue)
     {
         EndState = false;
-
-        PlayAnimation("CombatSequence");
-        //m_index++;
-        //if (m_index >= 2)
-        //    m_index = 0;
-        //switch(_poiseValue)
-        //{
-        //    case >= 10:
-        //        break;
-        //    case <= -10:
-        //        break;
-        //    case <= -4:
-        //        if(m_index == 0)
-        //        {
-        //            PlayAnimation("EnemyAdvantageOne");
-        //            Debug.Log("enemyAttackOne");
-        //            break;
-        //        }                
-        //        PlayAnimation("EnemyAdvantageTwo");
-        //        Debug.Log("enemyAttackTwo");
-        //        break;
-        //    case >= 4:
-        //        if (m_index == 0)
-        //        {
-        //            PlayAnimation("PlayerAdvantageOne");
-        //            break;
-        //        }
-        //        PlayAnimation("PlayerAdvantageTwo");
-        //        break;            
-        //    default:
-        //        if (m_index == 0)
-        //        {
-        //            PlayAnimation("NeutralOne");
-        //            break;
-        //        }
-        //        PlayAnimation("NeutralTwo");
-        //        break;
-        //}
-    }    
+        switch(_poiseValue)
+        {
+            case >= 10:
+                break;
+            case <= -10:
+                break;            
+            default:
+                PlayAnimation("CombatSequence");
+                break;
+        }
+    }
 }
