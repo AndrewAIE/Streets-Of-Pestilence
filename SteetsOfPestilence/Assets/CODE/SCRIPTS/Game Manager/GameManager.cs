6590d26f6566c9ed501e1fa6b239e57a82f7f132
AndrewAIE <126208967+AndrewAIE@users.noreply.github.com>
2024-12-04 16:54:38 +1030

    removed cursor

--- SteetsOfPestilence/Assets/CODE/SCRIPTS/Game Manager/GameManager.cs
+++ SteetsOfPestilence/Assets/CODE/SCRIPTS/Game Manager/GameManager.cs
@@ -75,8 +75,9 @@ namespace Management
         // Start is called before the first frame update
         void Start()
         {
+            Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
-
+            
             //trigger cutscene if not played yet
             /*if (!_hasTriggeredCutscene)
                 Debug.Log("Triggered Cutscene");
